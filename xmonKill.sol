836d12a378b9632f6c985473d52be5b149be429c
linggauzzz <136638896+linggauzzz@users.noreply.github.com>
2023-06-15 02:31:59 +0700

    Update xmonKill.sol

--- xmonKill.sol
+++ xmonKill.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "../IERC20.sol";
-import "../SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 
 
